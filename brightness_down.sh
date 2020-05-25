0531d31c592d0d58d2bd6d0e662b6f66d9ef7c36
Amol Kapoor <theahura@gmail.com>
2020-05-24 20:45:59 -0700

    Create brightness_down.sh

---
+++ brightness_down.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CURRBRIGHT=$(xrandr --current --verbose | grep -m 1 'Brightness:' | cut -f2- -d:)
+NEWBRIGHT=$(echo "$CURRBRIGHT - 0.1" | bc)
+DISPLAYS=$(xrandr --current --verbose | grep ' connected' | cut -c 1-4)
+
+for display in $DISPLAYS; do
+echo $NEWBRIGHT
+xrandr --output $display --brightness $NEWBRIGHT
+done
