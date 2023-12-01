84101c078acc4b9638e7ed5d23d13199ca72a93e
Amol Kapoor <theahura@gmail.com>
2023-12-01 16:50:08 +0000

    Fix for new machine

--- install.sh
+++ install.sh
@@ -4,27 +4,25 @@ set -e
 sudo add-apt-repository ppa:jonathonf/vim
 
 sudo apt update
-sudo apt install vim-gnome
-sudo apt install tmux
-sudo apt install curl
-sudo apt install silversearcher-ag
+sudo apt install -y vim-gnome tmux curl silversearcher-ag
 
 # Python.
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 # Node and nvm
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install -y nodejs npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 
 # Setup.
 cd ~
-mkdir /home/$USER/code
+mkdir -p /home/$USER/code
 
 # xcape.
 cd /home/$USER/code
 sudo apt-get install git gcc make pkg-config libx11-dev libxtst-dev libxi-dev
-git clone https://github.com/alols/xcape.git
+if ! git clone https://github.com/alols/xcape.git xcape 2>/dev/null && [ -d "xcape" ] ; then
+    echo "Clone failed because the folder xcape exists"
+fi
 cd xcape
 make
 sudo make install
@@ -33,27 +31,30 @@ sudo make install
 # Pathogen.
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # Vundle.
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if ! git clone https://github.com/VundleVim/Vundle.vim.git xcape 2>/dev/null && [ -d "Vundle" ] ; then
+    echo "Clone failed because the folder Vundle exists"
+fi
 # Plugin install.
 vim +PluginInstall +qall
 # Formatters.
 pip install yapf
 sudo npm install -g prettier
+sudo npm install -g @volar/vue-language-server
 # Syntax.
-cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 pip install flake8
 sudo npm install -g eslint
 # YCM.
-sudo apt install build-essential cmake vim-nox python3-dev
-sudo apt install mono-complete golang nodejs default-jdk npm
+sudo apt install -y build-essential cmake vim-nox python3-dev mono-complete golang nodejs default-jdk npm
 cd ~/.vim/bundle/YouCompleteMe && python3 install.py --all
 
 # Vifm
-sudo apt install vifm
+sudo apt install -y vifm
 
 # Copy dotfiles.
 cd /home/$USER/code
-git clone git@github.com:theahura/configs.git
+if ! git clone git@github.com:theahura/configs.git configs 2>/dev/null && [ -d "configs" ] ; then
+    echo "Clone failed because the folder configs exists"
+fi
 cd configs
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
@@ -61,3 +62,6 @@ cp .eslintrc ~/.eslintrc
 cp .bashrc ~/.bashrc
 cp -R .config ~/.config
 cp -R .vifm ~/.vifm
+
+# Get the initial node version.
+nvm install 17
